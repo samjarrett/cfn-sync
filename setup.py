c8b31194621f4cb9b1abe518c01a52bfc1627c00
Sam Jarrett <samstah@gmail.com>
2020-11-10 20:09:22 +1100

    Fix dev-requirements

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
                 "Programming Language :: Python :: 3.6",
                 "Programming Language :: Python :: 3.7",
                 "Programming Language :: Python :: 3.8",
+                "Programming Language :: Python :: 3.9",
             ],
             packages=find_packages(exclude=["tests"]),
             include_package_data=True,
