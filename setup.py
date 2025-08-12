c9f5f7bab5a66b6238855ba5183b1c515a0d8db6
Sam Jarrett <samstah@gmail.com>
2025-08-12 23:11:40 +0900

    update python versions

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ Setup script.
 from setuptools import find_packages, setup
 
 if __name__ == "__main__":
-    with open("requirements.in", "r", encoding="utf-8") as requirements, open(
-        "README.rst", "r", encoding="utf-8"
-    ) as readme:
+    with (
+        open("requirements.in", "r", encoding="utf-8") as requirements,
+        open("README.rst", "r", encoding="utf-8") as readme,
+    ):
         setup(
             name="cfn-sync",
             use_scm_version=True,
@@ -20,15 +21,17 @@ if __name__ == "__main__":
                 "License :: OSI Approved :: MIT License",
                 "Programming Language :: Python",
                 "Programming Language :: Python :: 3",
-                "Programming Language :: Python :: 3.8",
                 "Programming Language :: Python :: 3.9",
                 "Programming Language :: Python :: 3.10",
+                "Programming Language :: Python :: 3.11",
+                "Programming Language :: Python :: 3.12",
+                "Programming Language :: Python :: 3.13",
             ],
             packages=find_packages(exclude=["tests"]),
             include_package_data=True,
             package_data={"cfn_sync": ["py.typed"]},
             entry_points={"console_scripts": ["cfn-sync = cfn_sync:main"]},
-            python_requires=">=3.8",
+            python_requires=">=3.9",
             setup_requires=["setuptools >= 18.0", "setuptools_scm"],
             install_requires=requirements.readlines(),
             test_suite="tests",
